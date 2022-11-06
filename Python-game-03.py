a8b45df92c84aee6a9498e2391c50224521fec7c
Rudra-Karangutkar <112561053+Rudra-Karangutkar@users.noreply.github.com>
2022-11-06 11:57:53 +0530

    Add files via upload

---
+++ Python-game-03.py
@@ -0,0 +1,29 @@
+from tkinter import scrolledtext
+
+
+def check_guess(guess, answer):
+    global score
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print("Correct Answer")
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input("Sorry Wrong Answer, try again:\n")
+            attempt = attempt + 1
+    if attempt == 3:
+        print(f"The Correct answer is {answer}")
+score = 0 
+print("Guess the Animal")
+guess1 = input("Which bear lives at the North Pole?\n")
+check_guess(guess1,"pollar bear")
+guess2 = input("Which is the fastest land animal?\n")
+check_guess(guess2,"Cheetah")
+guess3 = input("Which is the larget animal?\n")
+check_guess(guess3,"Blue Whale")
+guess4 = input("Which is largest bird in the World?\n")
+check_guess(guess4,"Ostrich")
+print("Your Score is "+ str(score))
